2b6145845b5730795a05aadea0ce8f3efb1c4ee3
Niall Donnelly <niall.donnelly@feedhenry.com>
2015-12-04 16:46:00 +0000

    Added A Fix For Stale Jobs Running After A More Up-To-Date Job Has Completed

--- test/agenda.js
+++ test/agenda.js
@@ -390,6 +390,62 @@ describe('Agenda', function() {
       });
     });
   });
+
+  describe('Scheduling Concurrent Jobs', function() {
+
+    beforeEach(function(done) {
+      clearJobs(done);
+    });
+
+    it('do not run more than once in a scheduled time interval', function(done) {
+      var jobs = new Agenda({
+        defaultConcurrency: 1,
+        db: {
+          address: mongoCfg
+        }
+      });
+      var jobRunInterval = 400;
+      var jobRunTime = 200;
+      var processEvery = 300;
+      var successTime = 5000;
+      var lastRunAt;
+
+      function jobProcessor(job, done){
+        if(lastRunAt){
+          var timeSinceLastRun = new Date() - lastRunAt;
+
+          if(timeSinceLastRun < jobRunInterval - 50 || timeSinceLastRun > jobRunInterval + 50){
+            throw "INVALID Job Execution Time " + timeSinceLastRun;
+          }
+        }
+
+        lastRunAt = new Date();
+
+        setTimeout(function(){
+          done();
+        }, jobRunTime);
+      }
+
+
+      jobs.define('concjob', {concurrency: 1}, jobProcessor);
+      var interval = setInterval(function(){
+        clearInterval(interval);
+        jobs.stop(done);
+      }, successTime);
+
+      jobs.on('fail', function(err){
+        clearInterval(interval);
+        jobs.stop(function(){
+          done(err);
+        });
+      });
+
+      this.timeout(11000);
+      jobs.every( jobRunInterval, 'concjob' );
+      jobs.processEvery(processEvery);
+      jobs.start();
+    });
+  });
 });
 
 describe('Job', function() {
